0f8bc8a2c8220d1e7df0983c67dcbb5071b4da86
Onias22 <j50635537@gmail.com>
2026-04-06 09:58:14 -0300

    Criação da classe Doador

---
+++ src/model/Doador.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Doador extends Usuario {
+    public Doador(int id, String nome, String telefone, String email, String endereco){
+        super(id, nome, telefone, email, endereco);
+    }
+}
